2963cde1d19e3af4279442a4f67e4c0224341c42
Mubashshir <ahmubashshir@gmail.com>
2022-06-10 00:54:56 +0600

    fix: Prevent segfaults caused by `emulate`d functions
    Recently introduced flag `-c` for `functions` builtin is used to copy
    one function to another, but because of an incorrect null check in
    `sticky_emulation_dup` it makes zsh segfault when copying a function
    defined with sticky emulation(`emulate <shell> -c 'f() { :; }'`).
    
    Context: http://ix.io/3ZMR/irc - 2022-06-09 - irc://libera.chat/#zsh
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- zinsults.plugin.zsh
+++ zinsults.plugin.zsh
@@ -13,37 +13,60 @@ if
 then
 	builtin zcompile -Mz "${__zinsult_msgfile}"
 fi
-if ! ((${+functions[command_not_found_handler]})); then
-	function command_not_found_handler {
-		printf 'zsh: command not found: %s\n' "$1" >&2
-	}
+if (( ! ${+zinsults_load} ))
+then typeset -ga zinsults_load
 fi
-if ! ((${+functions[__zinsult_try_find_command]}));then
-	functions -c command_not_found_handler __zinsult_try_find_command
-fi
-if (( ! ${+zinsults_load} )); then
-	typeset -ga zinsults_load
+
+# zsh segfaults on 'functions -c src dest' because of an incorrect null check & copy
+# in functions builtin for sticky shell emulation
+# Details: http://ix.io/3ZMR/irc - 2022-06-09 - irc://libera.chat/#zsh
+# Fixed in zsh-5.9-20-gd4955bc0f
+if ((${+functions[command_not_found_handler]}))
+then () {
+	setopt localoptions extendedglob noksharrays
+	local -a match
+	local -i 10 major minor patch
+	local -li 16 rev
+	local -F 3 version
+	[[ $ZSH_PATCHLEVEL = (#b)zsh-(*).(*)-(*)-g(*) ]] && ((
+		major = match[1],
+		minor = match[2],
+		patch = match[3],
+		rev   = 16#${match[4]},
+		version = ${match[1]}.${match[2]}
+	))
+	declare -r major minor patchrev version
+	if (("$1"))
+	then builtin functions -c "$2" "$3"
+	else builtin declare -g "functions[$3]=$functions[$2]"
+	fi
+} \
+		'version > 5.9 || version == 5.9 && (patch >= 20 || rev == 16#d4955bc)' \
+		command_not_found_handler \
+		__zinsult_try_find_command
 fi
 
 function command_not_found_handler {
-	if [[ ! -t 1 ]]; then
-		# Return if stdout is a pipe, not tty
-		return
+	if [[ ! -t 1 ]]
+	then return # Return if stdout is a pipe, not tty
 	fi
+
 	local -a msgs
 	local idx
 	setopt localoptions noksharrays
-	if ! ((${+CMD_NOT_FOUND_MSGS}));then
-		source "${__zinsult_msgfile}"
-	else
-		msgs=( "${CMD_NOT_FOUND_MSGS[@]}" )
+	if ! ((${+CMD_NOT_FOUND_MSGS}))
+	then source "${__zinsult_msgfile}"
+	else msgs=( "${CMD_NOT_FOUND_MSGS[@]}" )
 	fi
+
 	if (($#msgs>0));then
 		RANDOM=$(od -vAn -N4 -tu < /dev/urandom)
 		builtin print -P -f 'zsh: %s\n' "$msgs[RANDOM % $#msgs + 1]"
 		unset msgs
 	fi
-	__zinsult_try_find_command "$@"
+	if ((${+functions[__zinsult_try_find_command]}))
+	then __zinsult_try_find_command "$@"
+	else builtin print -P -f 'zsh: command not found: %s\n' "$1"
+	fi
 }
-
 # vim: ft=zsh ts=4 noet
