1ea07a3d1535d7343344176b73fec7f5c760225c
Mubashshir <ahmubashshir@gmail.com>
2023-04-17 16:21:10 +0600

    Add insults from sudo insults plugin
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- msgs.zsh
+++ msgs.zsh
@@ -85,6 +85,64 @@ if ((#zinsults_load == 0)) || [[ ${zinsults_load[(r)default]} == default ]];then
 	)
 fi
 
+if [[ ${zinsults_load[(r)sudo]} == sudo ]];then
+	msgs+=(
+		# ins_2001.h (2001 Space Odyssey insults)
+		"Just what do you think you're doing Dave?"
+		"It can only be attributed to human error."
+		"That's something I cannot allow to happen."
+		"My mind is going. I can feel it."
+		"Sorry about this, I know it's a bit silly."
+		"Take a stress pill and think things over."
+		"This mission is too important for me to allow you to jeopardize it."
+		"I feel much better now."
+		# ins_classic.h (Original Sudo 8 insults)
+		"Wrong!  You cheating scum!"
+		"And you call yourself a Rocket Scientist!"
+		"No soap, honkie-lips."
+		"Where did you learn to type?"
+		"Are you on drugs?"
+		"My pet ferret can type better than you!"
+		"You type like i drive."
+		"Do you think like you type?"
+		"Your mind just hasn't been the same since the electro-shock, has it?"
+		# ins_csops.h (CSOps insults)
+		"Maybe if you used more than just two fingers..."
+		"BOB says:  You seem to have forgotten your command, enter another!" # s/passwd/command/
+		"stty: unknown mode: doofus"
+		"I can't hear you -- I'm using the scrambler."
+		"The more you drive -- the dumber you get."
+		"Listen, broccoli brains, I don't have time to listen to this trash."
+		"Listen, burrito brains, I don't have time to listen to this trash."
+		"I've seen penguins that can type better than that."
+		"Have you considered trying to match wits with a rutabaga?"
+		"You speak an infinite deal of nothing"
+		# ins_goons.h (Goon Show insults)
+		"You silly, twisted boy you."
+		"He has fallen in the water!"
+		"We'll all be murdered in our beds!"
+		"You can't come in. Our tiger has got flu"
+		"I don't wish to know that."
+		"What, what, what, what, what, what, what, what, what, what?"
+		"You can't get the wood, you know."
+		"You'll starve!"
+		"... and it used to be so popular..."
+		"Pauses for audience applause, not a sausage"
+		"Hold it up to the light --- not a brain in sight!"
+		"Have a gorilla..."
+		"There must be cure for it!"
+		"There's a lot of it about, you know."
+		"You do that again and see what happens..."
+		"Ying Tong Iddle I Po"
+		"Harm can come to a young lad like that!"
+		"And with that remarks folks, the case of the Crown vs yourself was proven."
+		"Speak English you fool --- there are no subtitles in this scene."
+		"You gotta go owwwww!"
+		"I have been called worse."
+		"It's only your word against mine."
+		"I think ... err ... I think ... I think I'll go home"
+	)
+fi
 ### SHAKESPEARE INSULTS ###
 if [[ ${zinsults_load[(r)shakespeare]} == shakespeare ]];then
 	msgs+=(
